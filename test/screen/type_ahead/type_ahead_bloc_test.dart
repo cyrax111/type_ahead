1373dc1f4a95b1ec50df6a084ff26504ab26c8fd
cyrax <bskaxe@gmail.com>
2022-08-22 10:52:46 +0700

    feat: add bloc tests

---
+++ test/screen/type_ahead/type_ahead_bloc_test.dart
@@ -0,0 +1,84 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:type_ahead/screens/type_ahead/type_ahead_bloc.dart';
+import 'package:type_ahead/screens/type_ahead/type_ahead_model.dart';
+
+import '../../mock/navigation_mock.dart';
+import 'mock/type_ahead_repository_mock.dart';
+
+void main() {
+  group('TypeAheadBloc -', () {
+    blocTest<TypeAheadBloc, TypeAheadState>(
+      'Initialized',
+      build: () => TypeAheadBloc(
+          navigation: NavigationMock(),
+          typeAheadRepository: TypeAheadRepositoryMock()),
+      act: (bloc) => bloc.add(const InitializedEvent()),
+      expect: () => [
+        TypeAheadState(favorites: const {'2'}),
+      ],
+    );
+
+    blocTest<TypeAheadBloc, TypeAheadState>(
+      'Type "event"',
+      build: () => TypeAheadBloc(
+          navigation: NavigationMock(),
+          typeAheadRepository: TypeAheadRepositoryMock()),
+      act: (bloc) => bloc
+        ..add(const InitializedEvent())
+        ..add(const TypeAheadInputChangedEvent('event')),
+      expect: () => [
+        TypeAheadState(favorites: const {'2'}),
+        TypeAheadState(favorites: const {'2'}, isLoading: true),
+        TypeAheadState(
+          favorites: const {'2'},
+          events: [
+            TypeAheadRepositoryMock.event1,
+            TypeAheadRepositoryMock.event2
+          ],
+          typeAheadInput: const TypeAheadInput.dirty(value: 'event'),
+          allEventsLoaded: true,
+        ),
+      ],
+    );
+
+    blocTest<TypeAheadBloc, TypeAheadState>(
+      'Type "event" and select second event',
+      build: () => TypeAheadBloc(
+          navigation: NavigationMock(),
+          typeAheadRepository: TypeAheadRepositoryMock()),
+      act: (bloc) async {
+        bloc
+          ..add(const InitializedEvent())
+          ..add(const TypeAheadInputChangedEvent('event'));
+        await Future.delayed(const Duration(seconds: 1));
+        bloc.add(EventTappedEvent(TypeAheadRepositoryMock.event2));
+      },
+      expect: () => [
+        TypeAheadState(favorites: const {'2'}),
+        TypeAheadState(favorites: const {'2'}, isLoading: true),
+        TypeAheadState(
+          favorites: const {'2'},
+          events: [
+            TypeAheadRepositoryMock.event1,
+            TypeAheadRepositoryMock.event2
+          ],
+          typeAheadInput: const TypeAheadInput.dirty(value: 'event'),
+          allEventsLoaded: true,
+        ),
+        TypeAheadState(
+          favorites: const {'2'},
+          events: [
+            TypeAheadRepositoryMock.event1,
+            TypeAheadRepositoryMock.event2
+          ],
+          typeAheadInput: const TypeAheadInput.dirty(value: 'event'),
+          allEventsLoaded: true,
+          selectedEvent: TypeAheadRepositoryMock.event2,
+        ),
+      ],
+    );
+
+    // TODO(any): add bloc tests for another user cases
+  });
+}
